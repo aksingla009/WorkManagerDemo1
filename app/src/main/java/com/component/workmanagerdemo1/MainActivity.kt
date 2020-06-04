aa5ab242256933823bff3370ec7899a4800daa50
Amandeep Singla <aksingla009@gmail.com>
2020-06-04 15:01:07 +0530

    Onetime Work Request

--- app/src/main/java/com/component/workmanagerdemo1/MainActivity.kt
+++ app/src/main/java/com/component/workmanagerdemo1/MainActivity.kt
@@ -2,10 +2,24 @@ package com.component.workmanagerdemo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startBtn.setOnClickListener {
+            setOneTimeRequest()
+        }
+    }
+
+    private fun setOneTimeRequest(){
+        val oneTimeUploadRequest  =  OneTimeWorkRequest.Builder(UploadWorker::class.java).build()
+
+        WorkManager.getInstance(applicationContext).enqueue(oneTimeUploadRequest)
+
     }
 }
